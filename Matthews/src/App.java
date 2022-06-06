b681561a1199523b2520f54feca1a08a9cf4152a
F7021995 Matheus Ataides Reis <matheus.ataides@bb.com.br>
2022-06-06 08:47:19 -0300

    Estava alterando na porra da branch Dois... Que porra é essa.

--- Matthews/src/App.java
+++ Matthews/src/App.java
@@ -1,5 +1,14 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
+        calcular();
+    }
+
+    static int x = 0;
+    static int y = 0;
+
+    static void calcular() {
+        System.out.println("Only update in Dois... WHere is this updating?");
+        System.out.println(x * y);
     }
 }
